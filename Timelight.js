32a4de39714e0cf9db343b4f8e21a567e305e7c6
Pranangel <121978243+Pranangel@users.noreply.github.com>
2025-08-18 10:34:40 -0700

    Update Timelight.js
    Added line, circle, and rotate functions to drawClock method

--- Timelight.js
+++ Timelight.js
@@ -9,4 +9,23 @@ function windowResized() {
   resizeCanvas(windowWidth, windowHeight)
 }
 
-function drawClock() {}
+function drawClock() {
+  circle(0, 0, CLOCK_DIAMETER);
+  line(0, (CLOCK_DIAMETER/2)-40, 0, CLOCK_DIAMETER/2)     //12:00
+  line((CLOCK_DIAMETER/2)-40, 0, CLOCK_DIAMETER/2, 0)     //3:00
+  line(-((CLOCK_DIAMETER/2)-40), 0, -CLOCK_DIAMETER/2, 0) //6:00
+  line(0, -((CLOCK_DIAMETER/2)-40), 0, -CLOCK_DIAMETER/2) //9:00
+
+  rotate(PI+(PI/6))
+  line(0, (CLOCK_DIAMETER/2)-10, 0, CLOCK_DIAMETER/2)     //1:00
+  line((CLOCK_DIAMETER/2)-10, 0, CLOCK_DIAMETER/2, 0)     //4:00
+  line(0, -((CLOCK_DIAMETER/2)-10), 0, -CLOCK_DIAMETER/2) //7:00
+  line(-((CLOCK_DIAMETER/2)-10), 0, -CLOCK_DIAMETER/2, 0) //10:00
+
+  rotate(PI/6)
+  line(0, (CLOCK_DIAMETER/2)-10, 0, CLOCK_DIAMETER/2)     //2:00
+  line((CLOCK_DIAMETER/2)-10, 0, CLOCK_DIAMETER/2, 0)     //5:00
+  line(0, -((CLOCK_DIAMETER/2)-10), 0, -CLOCK_DIAMETER/2) //8:00
+  line(-((CLOCK_DIAMETER/2)-10), 0, -CLOCK_DIAMETER/2, 0) //11:00
+}
+
