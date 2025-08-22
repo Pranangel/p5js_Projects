2a22d9961daf8c02e475591f83f8cc5a0209ac3f
Pranangel <121978243+Pranangel@users.noreply.github.com>
2025-08-22 10:23:34 -0700

    Update Timelight.js
    Updated drawClock function to:
    1: Take in a color parameter (integer)
    2: Use the color parameter to color the clock.

--- Timelight.js
+++ Timelight.js
@@ -9,24 +9,38 @@ function windowResized() {
   resizeCanvas(windowWidth, windowHeight)
 }
 
-function drawClock() {
+
+function drawClock(color = 0) {
+  push();
+
+  if (color == abs(255-color)) {
+    fill(abs(255-color-50));
+  }
+  else {
+    fill(abs(255-color));
+  }
   circle(0, 0, CLOCK_DIAMETER);
-  line(0, (CLOCK_DIAMETER/2)-40, 0, CLOCK_DIAMETER/2)     //12:00
-  line((CLOCK_DIAMETER/2)-40, 0, CLOCK_DIAMETER/2, 0)     //3:00
+  stroke(color)
+  line(0, (CLOCK_DIAMETER/2)-40, 0, CLOCK_DIAMETER/2)   //12:00 (CLOCK_DIAMETER/2)-20, CLOCK_DIAMETER/2
+  line((CLOCK_DIAMETER/2)-40, 0, CLOCK_DIAMETER/2, 0)   //3:00
   line(-((CLOCK_DIAMETER/2)-40), 0, -CLOCK_DIAMETER/2, 0) //6:00
   line(0, -((CLOCK_DIAMETER/2)-40), 0, -CLOCK_DIAMETER/2) //9:00
 
   rotate(PI+(PI/6))
-  line(0, (CLOCK_DIAMETER/2)-10, 0, CLOCK_DIAMETER/2)     //1:00
-  line((CLOCK_DIAMETER/2)-10, 0, CLOCK_DIAMETER/2, 0)     //4:00
+  stroke(color)
+  line(0, (CLOCK_DIAMETER/2)-10, 0, CLOCK_DIAMETER/2)   //1:00 (CLOCK_DIAMETER/2)-5, CLOCK_DIAMETER/2
+  line((CLOCK_DIAMETER/2)-10, 0, CLOCK_DIAMETER/2, 0)   //4:00
   line(0, -((CLOCK_DIAMETER/2)-10), 0, -CLOCK_DIAMETER/2) //7:00
   line(-((CLOCK_DIAMETER/2)-10), 0, -CLOCK_DIAMETER/2, 0) //10:00
 
   rotate(PI/6)
-  line(0, (CLOCK_DIAMETER/2)-10, 0, CLOCK_DIAMETER/2)     //2:00
-  line((CLOCK_DIAMETER/2)-10, 0, CLOCK_DIAMETER/2, 0)     //5:00
+  stroke(color)
+  line(0, (CLOCK_DIAMETER/2)-10, 0, CLOCK_DIAMETER/2)   //2:00 (CLOCK_DIAMETER/2)-5, CLOCK_DIAMETER/2
+  line((CLOCK_DIAMETER/2)-10, 0, CLOCK_DIAMETER/2, 0)   //5:00
   line(0, -((CLOCK_DIAMETER/2)-10), 0, -CLOCK_DIAMETER/2) //8:00
   line(-((CLOCK_DIAMETER/2)-10), 0, -CLOCK_DIAMETER/2, 0) //11:00
+
+  pop();
 }
 
 function draw() {
@@ -35,4 +49,5 @@ function draw() {
   drawClock();
   pop();
 }
+
 
